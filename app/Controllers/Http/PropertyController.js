71e67a71d41eefe3cb8730abeb82ceb565f50d75
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2019-12-09 19:40:53 -0400

    finishing API

---
+++ app/Controllers/Http/PropertyController.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+class PropertyController {
+  async index ({ request }) {
+    
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query().with('images').nearBy(latitude, longitude, 10).fetch()
+
+    return properties
+  }
+
+  async store ({ auth, request, response }) {
+    
+    const { id } = auth.user
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
+  }
+
+  async show ({ params }) {
+
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+  async update ({ params, request, response }) {
+
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    return property
+  }
+
+  async destroy ({ params, auth, response }) {
+
+    const property = Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not Authorized!' })
+    }
+
+    await property.delete()
+  }
+}
+
+module.exports = PropertyController
--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,9 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.post('users', 'UserController.create')
+Route.post('sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController').apiOnly().middleware('auth')
+Route.post('properties/:id/images', 'ImageController.store').middleware('auth')
+Route.get('images/:path', 'ImageController.show')
